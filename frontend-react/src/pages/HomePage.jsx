51ca70ba9aa9518575c07792fb6010db2aa821f1
amitspiro <amitspiro98@gmail.com>
2025-01-03 15:51:28 +0200

    set btns home page

--- frontend-react/src/pages/HomePage.jsx
+++ frontend-react/src/pages/HomePage.jsx
@@ -1,7 +1,16 @@
 export function HomePage() {
     return (
         <section>
-            <h1>Home sweet Home</h1>
+            {/* <h1>Home sweet Home</h1> */}
+            <div>
+                <button>graphic</button>
+                <button>programming</button>
+                <button>marketing</button>
+                <button>animation</button>
+                <button>translation</button>
+                <button>music</button>
+                <button>consulting</button>
+            </div>
         </section >
     )
 }
