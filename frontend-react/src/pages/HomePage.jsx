9a53afec463ec3cfeebf208f4a4f8dd9f4fcecfc
amitspiro <amitspiro98@gmail.com>
2025-01-03 23:11:36 +0200

    create gigster pro ad

--- frontend-react/src/pages/HomePage.jsx
+++ frontend-react/src/pages/HomePage.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+
     
     export function HomePage() {
+        // const myImg='src\services\imgs\design.imgs\ad.jpeg'
         const navigate = useNavigate(); 
       
-        const handleButtonClick = () => {
-          navigate("/gig"); 
-        };
+        const handleNavigation = (destination) => {
+            navigate(destination); // Navigate to the specified destination
+          };
         return (
         <section>
             <div>
-                <button onClick={handleButtonClick}>graphic</button>
-                <button onClick={handleButtonClick}>programming</button>
-                <button onClick={handleButtonClick}>marketing</button>
-                <button onClick={handleButtonClick}>animation</button>
-                <button onClick={handleButtonClick}>translation</button>
-                <button onClick={handleButtonClick}>music</button>
-                <button onClick={handleButtonClick}>consulting</button>
+                <button onClick={() => handleNavigation('/gig')}>graphic</button>
+                <button onClick={() => handleNavigation('/gig')}>programming</button>
+                <button onClick={() => handleNavigation('/gigv')}>marketing</button>
+                <button onClick={() => handleNavigation('/gig')}>animation</button>
+                <button onClick={() => handleNavigation('/gig')}>translation</button>
+                <button onClick={() => handleNavigation('/gig')}>music</button>
+                <button onClick={() => handleNavigation('/gig')}>consulting</button>
             </div>
+            <div className="ad-pro"
+    ><img 
+    src="src\services\imgs\design.imgs\ad.jpeg"  
+    style={{ width: "100%" }} 
+  />
+      <button className="ad-btn" onClick={() => handleNavigation('about')}>Try Now</button>
+    </div>
         </section >
     )
 }
