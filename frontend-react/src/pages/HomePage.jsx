0f8a19af7d277823d871e219c0a8394d4a0e7119
amitspiro <amitspiro98@gmail.com>
2025-01-03 18:29:50 +0200

    set btns nevigation to explore

--- frontend-react/src/pages/HomePage.jsx
+++ frontend-react/src/pages/HomePage.jsx
@@ -1,15 +1,22 @@
-export function HomePage() {
-    return (
+import React from "react";
+import { useNavigate } from "react-router-dom";
+    
+    export function HomePage() {
+        const navigate = useNavigate(); 
+      
+        const handleButtonClick = () => {
+          navigate("/gig"); 
+        };
+        return (
         <section>
-            {/* <h1>Home sweet Home</h1> */}
             <div>
-                <button>graphic</button>
-                <button>programming</button>
-                <button>marketing</button>
-                <button>animation</button>
-                <button>translation</button>
-                <button>music</button>
-                <button>consulting</button>
+                <button onClick={handleButtonClick}>graphic</button>
+                <button onClick={handleButtonClick}>programming</button>
+                <button onClick={handleButtonClick}>marketing</button>
+                <button onClick={handleButtonClick}>animation</button>
+                <button onClick={handleButtonClick}>translation</button>
+                <button onClick={handleButtonClick}>music</button>
+                <button onClick={handleButtonClick}>consulting</button>
             </div>
         </section >
     )
