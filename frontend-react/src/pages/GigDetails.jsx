1e9e4ad7c0445e2452ca0a60e17a8d4d073fb032
hadar Avarjil <hadaravarjil@gmail.com>
2025-01-04 09:04:46 +0200

    add gig details to cmp

--- frontend-react/src/pages/GigDetails.jsx
+++ frontend-react/src/pages/GigDetails.jsx
@@ -9,7 +9,7 @@ import { loadGig, addGigMsg } from '../store/actions/gig.actions'
 
 export function GigDetails() {
 
-  const {gigId} = useParams()
+  const { gigId } = useParams()
   const gig = useSelector(storeState => storeState.gigModule.gig)
 
   useEffect(() => {
@@ -18,22 +18,28 @@ export function GigDetails() {
 
   async function onaddGigMsg(gigId) {
     try {
-        await addGigMsg(gigId, 'bla bla ' + parseInt(Math.random()*10))
-        showSuccessMsg(`Gig msg added`)
+      await addGigMsg(gigId, 'bla bla ' + parseInt(Math.random() * 10))
+      showSuccessMsg(`Gig msg added`)
     } catch (err) {
-        showErrorMsg('Cannot add gig msg')
-    }        
+      showErrorMsg('Cannot add gig msg')
+    }
 
-}
+  }
 
   return (
     <section className="gig-details">
       <Link to="/gig">Back to list</Link>
       <h1>Gig Details</h1>
       {gig && <div>
-        <h3>{gig.vendor}</h3>
-        <h4>${gig.price}</h4>
-        <pre> {JSON.stringify(gig, null, 2)} </pre>
+        <a>{gig.tags}</a>
+        <h2>{gig.title}</h2>
+        <img
+          className="avatar"
+          src={gig.owner.imgUrl}
+          alt={`${gig.owner.fullName} gig avatar`}
+        />
+        <h3>About This Gig</h3>
+        <p>{gig.description}</p>
       </div>
       }
       <button onClick={() => { onaddGigMsg(gig._id) }}>Add gig msg</button>
