e20ad2cda00c6f8343a6a73033544c1d943b8c37
Haimmichaeli90 <haimmichaeli95@gmail.com>
2025-01-04 11:32:05 +0200

    adding filter buttons and scss

--- frontend-react/src/cmps/GigList.jsx
+++ frontend-react/src/cmps/GigList.jsx
@@ -1,40 +1,43 @@
-import { userService } from '../services/user';
-import { GigPreview } from './GigPreview';
-import { useState, useEffect, useMemo } from 'react';
-import _ from 'lodash';
+import { userService } from '../services/user'
+import { GigPreview } from './GigPreview'
+import { useState, useEffect, useMemo } from 'react'
+import _ from 'lodash'
 
-export function GigList({ gigs, onRemoveGig, onUpdateGig, onAddGig }) {
-  const [sortBy, setSortBy] = useState(null);
-  const [sortOrder, setSortOrder] = useState('asc');
-  const [isVisible, setIsVisible] = useState(false);
+export function GigList({ gigs, onRemoveGig, onUpdateGig, onAddGig, isLoading }) {
+  const [sortBy, setSortBy] = useState(null)
+  const [sortOrder, setSortOrder] = useState('asc')
+  const [isVisible, setIsVisible] = useState(false)
+  const isFrom = "explore"
 
   useEffect(() => {
     // Delay to show animation when component is loaded
-    const timer = setTimeout(() => setIsVisible(true), 100);
-    return () => clearTimeout(timer);
-  }, []);
+    const timer = setTimeout(() => setIsVisible(true), 100)
+    return () => clearTimeout(timer)
+  }, [])
 
   const handleSort = (criteria) => {
     if (sortBy === criteria) {
-      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')
     } else {
-      setSortBy(criteria);
-      setSortOrder('asc');
+      setSortBy(criteria)
+      setSortOrder('asc')
     }
   };
 
   const toggleSortOrder = () => {
-    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
-  };
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')
+  }
 
   const sortedGigs = useMemo(() => {
-    return _.orderBy(gigs, sortBy, sortOrder);
-  }, [gigs, sortBy, sortOrder]);
+    return _.orderBy(gigs, sortBy, sortOrder)
+  }, [gigs, sortBy, sortOrder])
 
   function shouldShowActionBtns(gig) {
-    const user = userService.getLoggedinUser();
-    return user && (user.isAdmin || gig.owner?._id === user._id);
+    const user = userService.getLoggedinUser()
+    return user && (user.isAdmin || gig.owner?._id === user._id)
   }
+
+  if (isLoading) return <div className="loader">Loading...</div>
 
   return (
     <section className="main-gig-list">
@@ -46,7 +49,7 @@ export function GigList({ gigs, onRemoveGig, onUpdateGig, onAddGig }) {
           {sortOrder === 'asc' ? 'Ascending' : 'Descending'}
         </button>
       </div>
-      <div className={`gig-list ${isVisible ? 'show' : ''}`}>
+      <div className={`gig-list layout-row ${isVisible ? 'show' : ''}`}>
         {sortedGigs.map((gig) => (
           <GigPreview
             key={gig._id}
@@ -54,6 +57,7 @@ export function GigList({ gigs, onRemoveGig, onUpdateGig, onAddGig }) {
             onUpdateGig={onUpdateGig}
             gig={gig}
             canEdit={shouldShowActionBtns(gig)}
+            isFrom={isFrom} 
           />
         ))}
       </div>
--- frontend-react/src/pages/GigIndex.jsx
+++ frontend-react/src/pages/GigIndex.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect} from 'react'
-import { useSelector,useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { loadGigs, addGig, updateGig, removeGig } from '../store/actions/gig.actions'
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { gigService } from '../services/gig/'
@@ -7,48 +7,79 @@ import { GigList } from '../cmps/GigList'
 import { GigFilter } from '../cmps/GigFilter'
 
 export function GigIndex() {
-    const filterBy = useSelector((storeState) => storeState.gigModule.filterBy)
-    const gigs = useSelector((storeState) => storeState.gigModule.gigs)
-    const dispatch = useDispatch() 
- 
-    useEffect(() => {
-        loadGigs(filterBy).catch(() => showErrorMsg('Cannot load gigs'))
-    }, [filterBy]) 
+  const filterBy = useSelector((storeState) => storeState.gigModule.filterBy)
+  const gigs = useSelector((storeState) => storeState.gigModule.gigs)
+  const dispatch = useDispatch()
 
-    function onRemoveGig(gigId) {
-        removeGig(gigId)
-            .then(() => showSuccessMsg('Gig removed'))
-            .catch(() => showErrorMsg('Cannot remove gig'))
-    }
-    console.log('Hi')
+  const [currentPage, setCurrentPage] = useState(1)
+  const gigsPerPage = 12
 
-    function onAddGig() {
-        const gig = gigService.getEmptyGig()
-        gig.title = prompt('Title?')
-        addGig(gig)
-            .then((savedGig) => showSuccessMsg(`Gig added (id: ${savedGig._id})`))
-            .catch(() => showErrorMsg('Cannot add gig'))
-    }
+  // calc for future pagination
+  const totalPages = Math.ceil(gigs.length / gigsPerPage)
+  const startIndex = (currentPage - 1) * gigsPerPage
+  const currentGigs = gigs.slice(startIndex, startIndex + gigsPerPage)
 
-    function onUpdateGig(gig) {
-        const price = +prompt('New price?')
-        const gigToSave = { ...gig, price }
-        updateGig(gigToSave)
-            .then((savedGig) => showSuccessMsg(`Gig updated, new price: ${savedGig.price}`))
-            .catch(() => showErrorMsg('Cannot update gig'))
-    }
+  useEffect(() => {
+    loadGigs(filterBy).catch(() => showErrorMsg('Cannot load gigs'))
+  }, [filterBy])
 
-    function onSetFilter(filterBy) {
-        dispatch({ type: 'SET_GIGS_FILTER', filterBy }) 
-    }
+  function onRemoveGig(gigId) {
+    removeGig(gigId)
+      .then(() => showSuccessMsg('Gig removed'))
+      .catch(() => showErrorMsg('Cannot remove gig'))
+  }
 
-    return (
-        <main className="Gig-index main-layout">
-            <div className="gigless">
-                <GigFilter filterBy={filterBy} onSetFilter={onSetFilter} />
-                <GigList gigs={gigs} onRemoveGig={onRemoveGig} onUpdateGig={onUpdateGig} onAddGig={onAddGig} />
-            </div>
-        </main>
-    )
+  function onAddGig() {
+    const gig = gigService.getEmptyGig()
+    gig.title = prompt('Title?')
+    addGig(gig)
+      .then((savedGig) => showSuccessMsg(`Gig added (id: ${savedGig._id})`))
+      .catch(() => showErrorMsg('Cannot add gig'))
+  }
+
+  function onUpdateGig(gig) {
+    const price = +prompt('New price?')
+    const gigToSave = { ...gig, price }
+    updateGig(gigToSave)
+      .then((savedGig) => showSuccessMsg(`Gig updated, new price: ${savedGig.price}`))
+      .catch(() => showErrorMsg('Cannot update gig'))
+  }
+
+  function onSetFilter(filterBy) {
+    dispatch({ type: 'SET_GIGS_FILTER', filterBy })
+  }
+
+  function handlePageChange(newPage) {
+    setCurrentPage(newPage)
+  }
+
+  return (
+    <main className="gig-index main-layout">
+      <GigFilter filterBy={filterBy} onSetFilter={onSetFilter} />
+      {currentGigs.length ? (
+        <>
+          <GigList gigs={currentGigs} onRemoveGig={onRemoveGig} onUpdateGig={onUpdateGig} />
+          {/*    Pagination in future   */}
+          <div className="pagination">
+            {Array.from({ length: totalPages }, (_, i) => (
+              <button
+                key={i}
+                className={currentPage === i + 1 ? 'active' : ''}
+                onClick={() => handlePageChange(i + 1)}
+              >
+                {i + 1}
+              </button>
+            ))}
+          </div>
+        </>
+      ) : (
+        <section className="gigless">
+          <h2>We couldn't find Gigs that match your search</h2>
+          <button className="clr-filter" onClick={() => dispatch({ type: 'SET_GIGS_FILTER', filterBy: {} })}>
+            Clear all filters
+          </button>
+        </section>
+      )}
+    </main>
+  )
 }
-
