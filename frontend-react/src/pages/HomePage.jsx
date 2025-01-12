cc8c02d4519fe9bda172c895ad903a376273d642
amitspiro <amitspiro98@gmail.com>
2025-01-12 20:42:58 +0200

    failed attempt with fonts

--- frontend-react/src/pages/HomePage.jsx
+++ frontend-react/src/pages/HomePage.jsx
@@ -119,9 +119,8 @@ export function HomePage() {
 
      
       <div class="home-carousel-container">
-<h1> Popular Services</h1>
+<h1 style={{fontFamily:'macan-regular'}}> Popular Services</h1>
 
-  <button class="home-carousel-arrow left">⬅</button>
   <div class="home-carousel">
     <div class="popular-div light-green-div"><h3>Website <br/>Development</h3><div className="pop-inner-pic website-development-img"><AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1736551937/website-development_gig20i.webp'}/></div></div>
     <div class="popular-div orange-div"><h3>Logo Design</h3> <div className="pop-inner-pic logo-design-img"><AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1736551938/logo-design_xgy1r3.webp'}/></div></div>
@@ -136,7 +135,6 @@ export function HomePage() {
     <div class="Popular-div light-green-div"><h3>E-Commerce <br/> Marketing </h3><div className="pop-inner-pic e-commerce-marketing-img"><AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1736551934/e-commerce_ozwc08.webp'}/></div></div>
     <div class="Popular-div pink-div"><h3>Video Editing </h3><div className="pop-inner-pic video-editing-img"><AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1736551935/video-editing_xcfy43.webp'}/></div></div> */}
   </div>
-  <button class="home-carousel-arrow-right" id="home-carousel-arrow-right">➡</button>
 </div>
 
 
