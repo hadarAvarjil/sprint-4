e39f618b6800f70edf720c55c98f9eaf807c8da9
amitspiro <amitspiro98@gmail.com>
2025-01-18 03:18:37 +0200

    all done home except search respond header

--- frontend-react/src/cmps/CatDivs.jsx
+++ frontend-react/src/cmps/CatDivs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AddImg } from "../cmps/AddImg.jsx";
 import { useNavigate } from "react-router-dom";
 
-
+ 
 
 export function CatDivsContainer(){
     const navigate = useNavigate();
