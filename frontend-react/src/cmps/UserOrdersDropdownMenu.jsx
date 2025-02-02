969c473239a813c42ac7c23b27a464273b65368c
hadar Avarjil <hadaravarjil@gmail.com>
2025-02-02 23:41:41 +0200

    add loader, fix bugs, and add some scss

--- frontend-react/src/cmps/UserOrdersDropdownMenu.jsx
+++ frontend-react/src/cmps/UserOrdersDropdownMenu.jsx
@@ -3,38 +3,39 @@ import { userService } from "../services/user";
 import { AddImg } from "./AddImg";
 import { loadOrdersForDropDown } from "../store/actions/order.actions.js";
 import { useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
+import SvgIcon from "./SvgIcon.jsx";
 
-export function UserOrdersDropdownMenu({ loggedInUser, topOffset }) {
-  const dropdownOrders = useSelector((storeState) => storeState.orderModule.dropdownOrders) || [];
+export function UserOrdersDropdownMenu({ loggedInUser, topOffset, onClose }) {
+  const dropdownOrders = useSelector(
+    (storeState) => storeState.orderModule.dropdownOrders
+  ) || [];
   const [userOrders, setUserOrders] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Loader state
 
   useEffect(() => {
     if (dropdownOrders.length) {
       loadOrderData();
+    } else {
+      setIsLoading(false); // Stop loading if there are no orders
     }
-}, [dropdownOrders]);
- 
+  }, [dropdownOrders]);
 
   async function loadOrderData() {
     try {
+      setIsLoading(true); // Start loading
       const ordersData = await Promise.all(
-        dropdownOrders.map(async (order)=> {
+        dropdownOrders.map(async (order) => {
           try {
             const user = await userService.getById(order.sellerId);
-            console.log(order, "uouououou");
 
             return {
               ...order,
               fullName: user?.fullName || "Unknown Buyer",
-              imgUrl: user?.avatar || "/default-avatar.png", // Use default if missing
+              imgUrl: user?.avatar || "/default-avatar.png",
             };
           } catch (err) {
-            console.error(
-              `Error fetching user with ID ${order.sellerId}:`,
-              err
-            );
+            console.error(`Error fetching user with ID ${order.sellerId}:`, err);
             return {
               ...order,
               fullName: "Unknown Buyer",
@@ -47,6 +48,8 @@ export function UserOrdersDropdownMenu({ loggedInUser, topOffset }) {
       setUserOrders(ordersData);
     } catch (err) {
       console.error("Unexpected error while loading user orders:", err);
+    } finally {
+      setIsLoading(false); // Stop loading
     }
   }
 
@@ -54,7 +57,6 @@ export function UserOrdersDropdownMenu({ loggedInUser, topOffset }) {
     async function fetchOrders() {
       if (loggedInUser) {
         try {
-          console.log(loggedInUser);
           await loadOrdersForDropDown({ buyerId: loggedInUser._id });
         } catch (err) {
           console.error("Error loading orders:", err);
@@ -64,83 +66,79 @@ export function UserOrdersDropdownMenu({ loggedInUser, topOffset }) {
     fetchOrders();
   }, [loggedInUser]);
 
+  const statusColors = {
+    Pending: "#F1C40F",
+    "In Progress": "#3498DB",
+    Completed: "#2ECC71",
+    Delivered: "#9B59B6",
+    Rejected: "#E74C3C",
+  };
+
+  const getStatusElement = (status) => {
+    const color = statusColors[status] || "#F1C40F";
+    return (
+      <div
+        style={{
+          backgroundColor: color,
+          color: "#fff",
+          padding: "1px",
+          borderRadius: "4px",
+          fontWeight: "bold",
+          textAlign: "center",
+          width: "100px",
+        }}
+      >
+        {status}
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="dropdown-container">
+        <div className="user-orders-dropdown-menu">
+          <div className="dropdown-triangle"></div>
+          <div className="loader-container">
+            <div className="loader"></div>
+            <p className="loading-text">Loading Orders...</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   if (dropdownOrders.length === 0)
     return (
-      <div
-        className="user-orders-dropdown-menu"
-        style={{
-          top: `${topOffset}`,
-        }}
-      >
-        <div className="dropdown-triangle"></div>
-        <div className="no-orders-dropdown-container">
-          <div className="svg-wrap">
-            <AddImg picUrl="https://res.cloudinary.com/dtpewh2wk/image/upload/v1738343751/svg_xml_base64_PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA2NSA0MyI_PGcgaWQ9IjAySF9YWFNfT3ZlcnZpZXdfZW1wdHlfc3RhdGVfQWRtaW4iIHN0cm9rZT0ibm9uZSIgc3Ryb2tlLXdpZHRoPSIxIiBmaWxsPSJub25lIiBmaWx_ryte6e.svg"></AddImg>
-          </div>
+      <div className="dropdown-container">
+        <div
+          className="user-orders-dropdown-menu"
+          style={{
+            top: `${topOffset}`,
+          }}
+          onClick={onClose}
+        >
+          <div className="dropdown-triangle"></div>
+          <div className="no-orders-dropdown-container">
+            <div className="svg-wrap">
+              <AddImg picUrl="https://res.cloudinary.com/dtpewh2wk/image/upload/v1738343751/svg_xml_base64_PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA2NSA0MyI_PGcgaWQ9IjAySF9YWFNfT3ZlcnZpZXdfZW1wdHlfc3RhdGVfQWRtaW4iIHN0cm9rZT0ibm9uZSIgc3Ryb2tlLXdpZHRoPSIxIiBmaWxsPSJub25lIiBmaWx_ryte6e.svg"></AddImg>
+            </div>
 
-          <h3> No Orders Yet</h3>
-          <p>
-            Use the search box to find the digital <br /> services you need.
-          </p>
+            <h3>No Orders Yet</h3>
+            <p>
+              Use the search box to find the digital <br /> services you need.
+            </p>
+          </div>
         </div>
       </div>
     );
-  return (
-    <div
-      className="user-orders-dropdown-menu"
-      style={{
-        top: `${topOffset}`,
-        // right:`${rightOffset}`,
-      }}
-    >
-      <div className="dropdown-triangle"></div>
-      <div className="user-orders-container">
-        {userOrders.map((order) => {
-          const getStatusElement = (status) => {
-            if (status === "approved") {
-              return (
-                <div
-                  style={{
-                    backgroundColor: "green",
-                    color: "white",
-                    padding: "5px",
-                    borderRadius: "4px",
-                  }}
-                >
-                  Approved
-                </div>
-              );
-            } else if (status === "declined") {
-              return (
-                <div
-                  style={{
-                    backgroundColor: "red",
-                    color: "white",
-                    padding: "5px",
-                    borderRadius: "4px",
-                  }}
-                >
-                  Declined
-                </div>
-              );
-            } else {
-              return (
-                <div
-                  style={{
-                    color: "orange",
-                    padding: "5px",
-                    borderRadius: "4px",
-                  }}
-                >
-                  Pending
-                </div>
-              );
-            }
-          };
 
-          return (
-            <div className="user-orders-gig">
+  return (
+    <div className="dropdown-container">
+      <div className="user-orders-dropdown-menu" onClick={onClose}>
+        <div className="dropdown-triangle"></div>
+        <div className="user-orders-container">
+          {userOrders.map((order) => (
+            <div className="user-orders-gig" key={order._id}>
               <div className="orders-wrap">
                 <div className="order-img">
                   <Link to={`/gig/${order.gigId}`}>
@@ -149,34 +147,32 @@ export function UserOrdersDropdownMenu({ loggedInUser, topOffset }) {
                       alt="gigFirstImgUrl"
                       className="gigFirstImgUrl"
                     />
-                    {/* {order.title || "Unknown Gig"} */}
-                  </Link>{" "}
-                  {/* <img src={order.gigUrl} alt="Seller" className="seller" />{" "} */}
+                  </Link>
                 </div>
 
                 <div className="order-details">
                   <h3 style={{ color: "#404145" }}>
-                    {" "}
                     {order.title || "Unknown Gig Title"}
                   </h3>
 
                   <div className="order-details-info">
                     <p style={{ color: "#6b7280" }}>by {order.fullName}</p>
                     <div className="gig-status">
-                      {" "}
-                      {getStatusElement("order.status" || "pending")}
+                      {getStatusElement(order.orderState || "Pending")}
                     </div>
                   </div>
                 </div>
               </div>
               <hr className="divider" />
             </div>
-          );
-        })}
+          ))}
+        </div>
+
+        <div className="view-all-orders flex">
+          <Link to="/orders">View all orders</Link>
+          <SvgIcon iconName={"rightArrow"} />
+        </div>
       </div>
-      <Link to="/dashboard">
-        <div className="view-all-orders">  <Link to="/orders"></Link>View all orders {">"} </div>
-      </Link>
     </div>
   );
 }
