052ab50f6f693bffda55ae3007fbf911164c36ab
Haimmichaeli90 <haimmichaeli95@gmail.com>
2025-02-03 22:30:44 +0200

    build

--- backend/services/socket.service.js
+++ backend/services/socket.service.js
@@ -214,7 +214,7 @@ async function emitToUser({ type, data, userId }) {
 // If possible, send to all sockets BUT not the current socket
 // Optionally, broadcast to a room / to all
 async function broadcast({ type, data, room = null, userId }) {
-  userId = userId.toString()
+ 
   loggerService.info(`Broadcasting event: ${type}`)
   const excludedSocket = await _getUserSocket(userId)
   if (room && excludedSocket) {
--- frontend-react/src/cmps/UserMsg.jsx
+++ frontend-react/src/cmps/UserMsg.jsx
@@ -1,48 +1,100 @@
-import { eventBus } from '../services/event-bus.service'
+import { eventBus, showSuccessMsg } from '../services/event-bus.service'
 import { useState, useEffect, useRef } from 'react'
 import { socketService, SOCKET_EVENT_REVIEW_ABOUT_YOU } from '../services/socket.service'
 
 export function UserMsg() {
-    const [msg, setMsg] = useState(null)
-    const timeoutIdRef = useRef()
+	const [msg, setMsg] = useState(null)
+	const timeoutIdRef = useRef()
 
-    useEffect(() => {
-        const unsubscribe = eventBus.on('show-msg', (msg) => {
-            displayMsg(msg)
-        })
+	useEffect(() => {
+		const unsubscribe = eventBus.on('show-msg', msg => {
+			setMsg(msg)
+			if (timeoutIdRef.current) {
+				clearTimeout(timeoutIdRef.current)
+			}
+			timeoutIdRef.current = setTimeout(closeMsg, 3000)
+		})
 
-        socketService.on(SOCKET_EVENT_REVIEW_ABOUT_YOU, (review) => {
-            displayMsg({ txt: `New review about you: ${review.txt}`, type: 'success' })
-        })
+		// socketService.on(SOCKET_EVENT_REVIEW_ABOUT_YOU, review => {
+		// 	showSuccessMsg(`New review about me ${review.txt}`)
+		// })
 
-        return () => {
-            unsubscribe()
-            socketService.off(SOCKET_EVENT_REVIEW_ABOUT_YOU)
-        }
-    }, [])
+		// return () => {
+		// 	unsubscribe()
+		// 	socketService.off(SOCKET_EVENT_REVIEW_ABOUT_YOU)
+		// }
+	}, [])
 
-    function displayMsg(msg) {
-        setMsg(msg)
-        if (timeoutIdRef.current) {
-            clearTimeout(timeoutIdRef.current)
-        }
-        timeoutIdRef.current = setTimeout(closeMsg, 3000)
-    }
+	function closeMsg() {
+		setMsg(null)
+	}
 
-    function closeMsg() {
-        setMsg(null)
-    }
+	function msgClass() {
+		return msg ? 'visible' : ''
+	}
 
-    function msgClass() {
-        return msg ? 'visible' : ''
-    }
-
-    return (
-        msg && (
-            <section className={`user-msg ${msg?.type} ${msgClass()}`}>
-                <button onClick={closeMsg}>x</button>
-                {msg?.txt}
-            </section>
-        )
-    )
+	return (
+		// Conditionally render the section only if `msg` exists
+		// msg && (
+		// 	<section className={`user-msg ${msg?.type} ${msgClass()}`}>
+		// 		<button onClick={closeMsg}>x</button>
+		// 		{msg?.txt}
+		// 	</section>
+		// )
+		console.log('it is here')
+		
+	)
 }
+
+
+
+
+
+
+
+
+
+
+//shinoi
+// import { eventBus, showSuccessMsg } from '../services/event-bus.service'
+// import { useState, useEffect, useRef } from 'react'
+// import { socketService, SOCKET_EVENT_REVIEW_ABOUT_YOU } from '../services/socket.service'
+
+// export function UserMsg() {
+// 	const [msg, setMsg] = useState(null)
+// 	const timeoutIdRef = useRef()
+
+// 	useEffect(() => {
+// 		const unsubscribe = eventBus.on('show-msg', msg => {
+// 			setMsg(msg)
+// 			if (timeoutIdRef.current) {
+// 				timeoutIdRef.current = null
+// 				clearTimeout(timeoutIdRef.current)
+// 			}
+// 			timeoutIdRef.current = setTimeout(closeMsg, 3000)
+// 		})
+
+// 		socketService.on(SOCKET_EVENT_REVIEW_ABOUT_YOU, review => {
+// 			showSuccessMsg(`New review about me ${review.txt}`)
+// 		})
+
+// 		return () => {
+// 			unsubscribe()
+// 			socketService.off(SOCKET_EVENT_REVIEW_ABOUT_YOU)
+// 		}
+// 	}, [])
+
+// 	function closeMsg() {
+// 		setMsg(null)
+// 	}
+
+//     function msgClass() {
+//         return msg ? 'visible' : ''
+//     }
+// 	return (
+// 		<section className={`user-msg ${msg?.type} ${msgClass()}`}>
+// 			<button onClick={closeMsg}>x</button>
+// 			{msg?.txt}
+// 		</section>
+// 	)
+// }
