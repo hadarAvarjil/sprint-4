930f00059091e9969cbf7a9651e9eb141c9d4379
amitspiro <amitspiro98@gmail.com>
2025-02-04 04:09:25 +0200

    fix popular service bug

--- frontend-react/src/cmps/PopularServicesDiv.jsx
+++ frontend-react/src/cmps/PopularServicesDiv.jsx
@@ -52,73 +52,73 @@ export function PopularServices() {
             
             <div onClick={() => setCatFilter('Programming & Tech')} to={`/gig`} 
              className="popular-div light-green-div first-pop-div">
-              <h3 className="pop-div-title">Website Development</h3>
+              <h3 className="pop-div-title">Programming & Tech</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.56_1_pyspmu.jpg'}/>
               </div>
             </div>
             <div onClick={() => setCatFilter('Graphics & Design')} to={`/gig`} className="popular-div orange-div">
-              <h3 className="pop-div-title">Logo Design</h3>
+              <h3 className="pop-div-title">Graphics & Design</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.55_2_hckvzx.jpg'}/>
               </div>
             </div>
             <div onClick={() => setCatFilter('AI Services')} to={`/gig`} className="popular-div dark-green-div"> 
-              <h3 className="pop-div-title">SEO</h3>
+              <h3 className="pop-div-title">AI Services</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.54_1_hivb1p.jpg'}/>
               </div>
             </div>
             <div onClick={() => setCatFilter('Graphics & Design')} to={`/gig`} className="popular-div purple-div">
-              <h3 className="pop-div-title">Architecture & Interior Design</h3>
+              <h3 className="pop-div-title">Graphics & Design</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.54_eofkkc.jpg'}/>
               </div>
             </div>
             <div onClick={() => setCatFilter('Digital Marketing')} to={`/gig`} className="popular-div green-shrek-div">
-              <h3 className="pop-div-title">Social Media Marketing</h3>
+              <h3 className="pop-div-title">Digital Marketing</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.55_1_pxgamn.jpg'}/>
               </div>
             </div>
             <div onClick={() => setCatFilter('Music & Audio')} to={`/gig`} className="popular-div brown-div">
-              <h3 className="pop-div-title">Voice Over</h3>
+              <h3 className="pop-div-title">Music & Audio</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.53_xq2w2p.jpg'}/>
               </div>
             </div>
             <div onClick={() => setCatFilter('Digital Marketing')} to={`/gig`} className="popular-div pink-div">
-              <h3 className="pop-div-title">UGC Videos</h3>
+              <h3 className="pop-div-title">Digital Marketing</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.53_1_r1e8aj.jpg'}/>
               </div>
             </div>
             <div onClick={() => setCatFilter('Programming & Tech')} to={`/gig`} className="popular-div green-swamp-div">
-              <h3 className="pop-div-title">Software Development</h3>
+              <h3 className="pop-div-title">Programming & Tech</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.53_2_ndgpxw.jpg'}/>
               </div>
             </div>
             <div onClick={() => setCatFilter('Data')} to={`/gig`} className="popular-div light-brown-div">
-              <h3 className="pop-div-title">Data Science & ML</h3>
+              <h3 className="pop-div-title">Data</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.55_zcb4lf.jpg'}/>
               </div>
             </div>
             <div onClick={() => setCatFilter('Photography')} to={`/gig`} className="popular-div green-shrek-div">
-              <h3 className="pop-div-title">Product Photography</h3>
+              <h3 className="pop-div-title">Photography</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.54_2_st7oyu.jpg'}/>
               </div>
             </div>
             <div onClick={() => setCatFilter('Digital Marketing')} to={`/gig`} className="popular-div light-green-div">
-              <h3 className="pop-div-title">E-Commerce Marketing</h3>
+              <h3 className="pop-div-title">Digital Marketing</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.55_3_aagztg.jpg'}/>
               </div>
             </div>
             <div onClick={() => setCatFilter('Video & Animation')} to={`/gig`} className="popular-div pink-div">
-              <h3 className="pop-div-title">Video Editing</h3>
+              <h3 className="pop-div-title">Video & Animation</h3>
               <div className="pop-inner-pic">
               <AddImg picUrl={'https://res.cloudinary.com/dtpewh2wk/image/upload/v1737284056/WhatsApp_Image_2025-01-19_at_12.51.56_zhxfha.jpg'}/>
               </div>
